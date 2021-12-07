c81050763dd1b52df2f98c78f6797657efbc5b82
ivnmnndz <12gues@gmail.com>
2021-12-06 20:06:25 -0500

    global state test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import { GlobalProvider } from "./context/GlobalState";
 import Signup from "../src/components/Signup";
 
 function App() {
   return (
-  <div>
-  hello
-    <Signup />
-  </div>
+    <GlobalProvider>
+      <Signup />
+    </GlobalProvider>
   );
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 const Signup = () => {
+  const { state } = useContext(GlobalContext);
   return (
+    <>
       <form>
+      <label>
+      Email
         <input className="border-black rounded-md" name="email" />
+        </label>
+        <label>
+        Password
         <input name="password" />
+        </label>
       </form>
+      <div>
+        {state}
+      </div>
+      </>
   );
 }
 export default Signup;
