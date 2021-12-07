77d24e95fd5adf41cdba7ed800914d13db066784
cgarzon44 <83254540+cgarzon44@users.noreply.github.com>
2021-12-07 17:18:51 -0500

    push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import "../css/Navbar.css";
 
 const Navbar = () => {
   return (
-<nav className="bg-gray-800">
-    <div>Logo</div>
+<nav className="navBar">
+   
+    <div className="NavBarDiv">
+ 
+      <div>Logo</div>
+    
+      <a href="">Home</a>
+   
+    </div>
+    <div className="LogInSignUpDiv" >
+    <a href="">LogIn</a>
+   
+      <a href="">SignUp</a>
+    </div>
+
 </nav>
   )
 }
