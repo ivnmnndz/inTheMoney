870d27aa91c7e8f5db09f17a8060a9ccae662a00
ivnmnndz <12gues@gmail.com>
2021-12-06 21:03:24 -0500

    starting signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,24 +1,33 @@
-import React, { useContext } from 'react'
-import { GlobalContext } from '../context/GlobalState';
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const Signup = () => {
-  const { state } = useContext(GlobalContext);
+  const { setState } = useContext(GlobalContext);
+
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault;
+    setState(values);
+  }
+
   return (
     <>
-      <form>
-      <label>
-      Email
-        <input className="border-black rounded-md" name="email" />
-        </label>
+      <form onSubmit={handleSubmit}>
         <label>
-        Password
-        <input name="password" />
+          Email
+          <input className="border-black rounded-md" name="email" type="text" />
+        </label>
+
+        <label>
+          Password
+          <input name="password" type="text" />
         </label>
       </form>
-      <div>
-        {state}
-      </div>
-      </>
+    </>
   );
-}
+};
 export default Signup;
