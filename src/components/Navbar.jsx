de8e7f20a66dcf5ce483c20ab407d11352b81d55
ivnmnndz <12gues@gmail.com>
2021-12-07 16:22:07 -0500

    -tailwind

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+<nav className="bg-gray-800">
+    <div>Logo</div>
+</nav>
+  )
+}
+
+export default Navbar
---
+++ src/views/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return user ? <div>signed in</div> : <div>no user</div>;
+};
+
+export default Home;
