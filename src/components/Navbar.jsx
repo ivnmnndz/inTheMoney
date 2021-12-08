cd60e4cafaf19788144646c3d167f237f41d451f
cgarzon44 <83254540+cgarzon44@users.noreply.github.com>
2021-12-08 11:23:41 -0500

    Nav-bar-Styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,13 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="nav-links">
-        <Link to="/" className="logo">💰</Link>
-      </div>
+     <div className="nav-links">
+        <Link id='nav-link' to="/" className="logo">💰</Link>
+     
+ </div>
       <div className="login-signup">
-        <Link to="/login">Log In</Link>
-        <Link to="/signup">Sign Up</Link>
+        <Link id='nav-link'to="/login">Log In</Link>
+        <Link id='nav-link'to="/signup">Sign Up</Link>
       </div>
     </nav>
   );
