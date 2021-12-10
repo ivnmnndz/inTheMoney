20dce29f66dcc7b1b266cbb1c87e1e3fa7bdc734
cgarzon44 <83254540+cgarzon44@users.noreply.github.com>
2021-12-10 02:20:24 -0500

    updates

--- src/components/Home/HomePageNull.jsx
+++ src/components/Home/HomePageNull.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import "../../css/LoggedOutHome.css";
+import "../../css/HomePageNull.css";
 import { Link } from "react-router-dom";
 
 function LoggedOutHome() {
   return (
-    <div className="HomePage-container">
-      <h1 className="inTheMoney-logo">
+    <div className="homePage-container">
+      <h1 className="solgan">
         Buy & Sell <i className="fab fa-bitcoin"></i> Today!
       </h1>
 
       <div className="signup-login-container">
         <Link to="/signup">
-          <button type="button" id="Create-User-btn">
+          <button type="button" id="create-User-btn">
             Create Your Account
           </button>
         </Link>
 
-        <Link to="/login">
-          <p>Already have an account? Log in here!</p>
+        <Link id="logIn-link" to="/login">
+          Already have an account? Log in here!
         </Link>
       </div>
     </div>
