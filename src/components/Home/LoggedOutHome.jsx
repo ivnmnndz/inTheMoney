3eaf5aee3764d2d2d65513c792c102029963ca47
cgarzon44 <83254540+cgarzon44@users.noreply.github.com>
2021-12-09 21:36:10 -0500

    homepage-styles

--- src/components/Home/LoggedOutHome.jsx
+++ src/components/Home/LoggedOutHome.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import "../../css/LoggedOutHome.css"
+import "../../css/LoggedOutHome.css";
 import { Link } from "react-router-dom";
 
 function LoggedOutHome() {
   return (
     <div className="HomePage-container">
-      <h1>
-        In the Money<small>₿</small>
+      <h1 className="inTheMoney-logo">
+        In the Money
       </h1>
 
       <div className="signup-login-container">
-        <div>
-          <Link to="/signup">
-            <button id="Create-User-btn">Create Your Account</button>
-          </Link>
-        </div>
+        <Link to="/signup">
+          <button type="button" id="Create-User-btn">
+            Create Your Account
+          </button>
+        </Link>
+
         <Link to="/login">
-          <small>Already have an account? Log in here!</small>
+          <p>Already have an account? Log in here!</p>
         </Link>
       </div>
     </div>
