10765d911a60379084119795dea64430f3539d29
ivnmnndz <59851853+ivnmnndz@users.noreply.github.com>
2022-01-17 10:57:54 -0500

    footer and faq (#31)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ForgotPassword from "./views/ForgotPassword";
 import Signin from "./components/Login/Signin";
 import Signup from "./components/Login/Signup";
 import Dashboard from "./components/Dashboard";
+import FAQ from "./views/FAQ";
 import CoinPage from "./components/CoinPage";
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
         <Route path="login" element={<Signin />} />
         <Route path="signup" element={<Signup />} />
         <Route path="profile" element={<Profile />} />
+        <Route path="FAQ" element={<FAQ />} />
         <Route path="dashboard" element={<Dashboard />} />
         <Route path=":id" element={<CoinPage />} />
       </Route>
---
+++ src/views/FAQ.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FAQ = () => {
+  return (
+    <div>
+      <h3>What is Market Cap?</h3>
+      <p>
+        Market Cap or market capitalization, in crypto, is the total value of
+        all the coins mined multiplied by the cost of a single coin
+      </p>
+    </div>
+  );
+};
+
+export default FAQ;
